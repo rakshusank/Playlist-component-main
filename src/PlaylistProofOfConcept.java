3cc0d09d0ce33cbf161c816333387161ea135c4d
rakshusank <rakshu.sank27@gmail.com>
2025-11-21 16:11:27 -0500

    Add files via upload

---
+++ src/PlaylistProofOfConcept.java
@@ -0,0 +1,31 @@
+import javax.sound.mi d
+    
+
+    private Sequence< S
+        
+    p
+
+    } 
+
+    /** adds songs to the playlist*/ 
+        
+    public void addSong(String song){
+    this.songs.add(this.songs.length(), song);
+  }
+
+  /** removes song from the playlist*/
+
+  public String removeSong(){
+   
+
+}
+
+  /** Returns number of songs in the playlist */
+
+
+return this.songs.length();
+
+    /** check for empty playlist*/
+
+
+
